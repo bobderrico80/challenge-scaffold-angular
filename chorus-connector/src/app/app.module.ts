9ebbe2934eb38222f359b9699a6c6015076fa926
Robert D'Errico <bobderrico80@gmail.com>
2024-10-29 07:37:38 -0400

    Install Angular Material

--- chorus-connector/src/app/app.module.ts
+++ chorus-connector/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ChorusComponent } from './components/chorus/chorus.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { LoginComponent } from './components/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
